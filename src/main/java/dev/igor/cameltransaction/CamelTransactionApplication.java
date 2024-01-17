bcecad7891807de5c40977f13282e988743aebf3
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2024-01-17 20:51:17 -0300

    start service

---
+++ src/main/java/dev/igor/cameltransaction/CamelTransactionApplication.java
@@ -0,0 +1,13 @@
+package dev.igor.cameltransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelTransactionApplication.class, args);
+	}
+
+}
